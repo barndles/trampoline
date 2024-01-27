8045462cadd10f9c3db4094301189cdf99a3c3e1
GameCrucible <cpdepalm@gmail.com>
2024-01-26 23:59:23 -0600

    Building Update 2
    Buildings now spawn with a randomized number of floors, they are also properly aligned and will remain aligned when scaling.

--- Scripts/building_scripts/building_builder.gd
+++ Scripts/building_scripts/building_builder.gd
@@ -1,18 +1,16 @@
 extends Node
 
+@onready var buildingSprite = $Sprite2D
 var buildingSpawn = preload("res://Scenes/buildings/building_orphanage.tscn")
-
+var rng = RandomNumberGenerator.new()
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	inst(Vector2(0,0))
-	inst(Vector2(0,130))
+	var random_num = rng.randi_range(1,3)
+	for n in random_num:
+		inst(Vector2(0,((n+1) * (-buildingSprite.texture.get_height()))))
 	pass # Replace with function body.
 
 func inst(pos):
 	var instance = buildingSpawn.instantiate();
 	instance.position = pos
 	add_child(instance)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
