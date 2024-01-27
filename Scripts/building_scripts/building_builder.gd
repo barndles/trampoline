9089b4d807682ce958b66b74719bb17423728c23
GameCrucible <cpdepalm@gmail.com>
2024-01-27 00:13:59 -0600

    Building Update 4
    Now the building stacks different scenes on itself, so every layout is randomized

--- Scripts/building_scripts/building_builder.gd
+++ Scripts/building_scripts/building_builder.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @onready var buildingSprite = $Sprite2D
+var array = [preload("res://Scenes/buildings/building_orphanage.tscn"), preload("res://Scenes/buildings/building_oldhome.tscn")]
 var buildingSpawn = preload("res://Scenes/buildings/building_orphanage.tscn")
 var rng = RandomNumberGenerator.new()
 # Called when the node enters the scene tree for the first time.
@@ -11,6 +12,8 @@ func _ready():
 	pass # Replace with function body.
 
 func inst(pos):
-	var instance = buildingSpawn.instantiate();
+	var rng_Floors = rng.randi_range(0,1)
+	var floor_select = array[rng_Floors]
+	var instance = floor_select.instantiate();
 	instance.position = pos
 	add_child(instance)
