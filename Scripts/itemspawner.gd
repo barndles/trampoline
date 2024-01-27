588cc814ce18fa4cdc3eb1250eaa911652539bcf
GameCrucible <cpdepalm@gmail.com>
2024-01-27 16:31:44 -0600

    Item Spawner Update
    Babies now spawn at each window

--- Scripts/itemspawner.gd
+++ Scripts/itemspawner.gd
@@ -1,15 +1,15 @@
 extends Node
 
 var thrown_object = preload("res://Scenes/falling_object.tscn")
+var spawn_points = [Vector2(831,327),Vector2(959,327),Vector2(1087,327),Vector2(831,150),Vector2(959,150),Vector2(1087,150)]
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	inst(Vector2(872.33,200))
+	inst(spawn_points[0])
 	wait_seconds_to_spawn()
 
 func wait_seconds_to_spawn():
-	var rng_time = randf_range(3.0,8.0)
-	await get_tree().create_timer(rng_time).timeout
-	inst(Vector2(872.33,200))
+	await get_tree().create_timer(randf_range(3.0,8.0)).timeout
+	inst(spawn_points[randi_range(0,5)]) 
 	wait_seconds_to_spawn()
 
 func inst(pos):
