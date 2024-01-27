50f6bf5559d6576edd5e5e5de00f3a127b262554
GameCrucible <cpdepalm@gmail.com>
2024-01-26 23:41:52 -0600

    Building Update
    BUILDINGS NOW GENERATE PROPELRY WOOOOO

--- Scripts/building_scripts/building_builder.gd
+++ Scripts/building_scripts/building_builder.gd
@@ -4,10 +4,14 @@ var buildingSpawn = preload("res://Scenes/buildings/building_orphanage.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var instance = buildingSpawn.instantiate();
-	add_child(instance)
+	inst(Vector2(0,0))
+	inst(Vector2(0,130))
 	pass # Replace with function body.
 
+func inst(pos):
+	var instance = buildingSpawn.instantiate();
+	instance.position = pos
+	add_child(instance)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
