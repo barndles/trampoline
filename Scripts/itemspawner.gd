c087e5897d3f1dd70233c527db983079886a82da
GameCrucible <cpdepalm@gmail.com>
2024-01-27 15:42:23 -0600

    ITEM SPAWNER FIX
    Now items should spawn properly

--- Scripts/itemspawner.gd
+++ Scripts/itemspawner.gd
@@ -3,12 +3,12 @@ extends Node
 var thrown_object = preload("res://Scenes/falling_object.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	inst(Vector2(500,200))
+	inst(Vector2(872.33,200))
 	wait_seconds_to_spawn()
 
 func wait_seconds_to_spawn():
-	await get_tree().create_timer(3.0).timeout
-	inst(Vector2(500,200))
+	await get_tree().create_timer(5.0).timeout
+	inst(Vector2(872.33,200))
 
 func inst(pos):
 	var instance = thrown_object.instantiate();
