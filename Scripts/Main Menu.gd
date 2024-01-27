f7dddd0e1f16697cee08c9f7230a2a11efbfc142
ButtersBo <matthew.sanstra@gmail.com>
2024-01-27 00:06:23 -0600

    Added Menu Music + Sound Effect on game start

--- Scripts/Main Menu.gd
+++ Scripts/Main Menu.gd
@@ -3,7 +3,8 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$"VBoxContainer/Start Game".grab_focus()
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,8 +13,14 @@ func _process(delta):
 
 
 func _on_start_game_pressed():
+	$VBoxContainer/Starting.play()
+	await get_tree().create_timer(1).timeout
 	get_tree().change_scene_to_file("res://Scenes/main_level.tscn")
 
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+func _on_settings_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Menus/Settings.tscn")
