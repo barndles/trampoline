b1917998cdd26a30963ceb08140706f02c8587d4
barndles <59572396+barndles@users.noreply.github.com>
2024-01-27 09:59:17 -0600

    falling objects have a vague sense of functionality now

--- Scripts/falling_object.gd
+++ Scripts/falling_object.gd
@@ -1,12 +1,24 @@
 extends Node2D
 
 @onready var fallingObject = $"."
+@onready var col = $CollisionShape2D
 
-var gravity = 1
+var random = RandomNumberGenerator.new()
+var randomVectorInRange = Vector2()
+
+var impulseMultiplier = 300
+
+
+var v = Vector2() #object velocity
+var gravity = 0.98
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	
+	randomVectorInRange = Vector2((random.randf()) * -1, random.randf() * -1) #random (0-1, 0-1) vector mirrored on each axis
+	print(randomVectorInRange)
+	fallingObject.apply_impulse(randomVectorInRange * impulseMultiplier)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
