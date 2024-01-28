5711f51142cb83cc503022384b9a6cd60bd8ada7
barndles <59572396+barndles@users.noreply.github.com>
2024-01-27 19:30:30 -0600

    game?

--- health.gd
+++ health.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
 @onready var global = get_node("/root/Autoload")
+var currentLives
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print(get_child(global.lives - 1).name)
+	currentLives = global.lives
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if currentLives != global.lives:
+		get_child(global.lives).texture = load("res://Sprites/Images/BlackCross.png")
