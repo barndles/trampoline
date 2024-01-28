4b8ccc379624158eff0ae89e1842754c5e11661e
ButtersBo <matthew.sanstra@gmail.com>
2024-01-27 23:55:07 -0600

    Intro

--- Scenes/FirstScene.gd
+++ Scenes/FirstScene.gd
@@ -3,11 +3,11 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	await get_tree().create_timer(2).timeout
+	
+	get_tree().change_scene_to_file("res://Scenes/main_level.tscn")
 
-func _input(event):
-	if event.is_action_pressed("Continue"):
-		get_tree().change_scene_to_file("res://Scenes/main_level.tscn")
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
