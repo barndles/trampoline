ceb50d7cdb08076ff3922b15ab4fccb7ec8f3a43
ButtersBo <matthew.sanstra@gmail.com>
2024-01-27 19:26:25 -0600

    Death

--- Scenes/Menus/game_over.gd
+++ Scenes/Menus/game_over.gd
@@ -1,12 +1,13 @@
 extends Control
 
-
+@onready var global = get_node("/root/Autoload")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$VBoxContainer/Restart.grab_focus()
 
 func _on_restart_pressed():
-		get_tree().change_scene_to_file("res://Scenes/main_level.tscn")
+	global.lives = 5
+	get_tree().change_scene_to_file("res://Scenes/main_level.tscn")
 	
 func _on_quit_pressed():
 	get_tree().quit()
