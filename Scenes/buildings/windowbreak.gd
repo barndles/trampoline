b97e9a39a37b30ab1f5a76aa9cc27bcb41d6ddb3
GameCrucible <cpdepalm@gmail.com>
2024-01-28 00:17:11 -0600

    CHANGES

---
+++ Scenes/buildings/windowbreak.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+@onready var window_sprite = get_node("Window")
+
+#func _ready():
+	#window_sprite.play("Broken")
+# Called when the node enters the scene tree for the first time.
+func on_window_entered(body):
+	window_sprite.play("broken")
+
